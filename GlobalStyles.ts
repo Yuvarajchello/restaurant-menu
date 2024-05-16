5a0366e9abb2633488c792eba6dabad54a280fcb
Yuvarajchello <148035131+Yuvarajchello@users.noreply.github.com>
2024-05-16 09:33:00 +0530

    Add files via upload

---
+++ GlobalStyles.ts
@@ -0,0 +1,24 @@
+/* fonts */
+export const FontFamily = {
+  interSemiBold: "Inter-SemiBold",
+  interBold: "Inter-Bold",
+  inriaSansBold: "InriaSans-Bold",
+};
+/* font sizes */
+export const FontSize = {
+  size_xs: 12,
+  size_5xl: 24,
+  size_lg: 18,
+  size_7xl: 26,
+};
+/* Colors */
+export const Color = {
+  colorWhite: "#fff",
+  colorBlack: "#000",
+  colorGray: "#8b8b8b",
+};
+/* border radiuses */
+export const Border = {
+  br_11xl: 30,
+  br_xl: 20,
+};
